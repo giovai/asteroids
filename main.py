c3774281155dd04e891d994c2f8c22813b7c31cf
Joanna Iwanska <joanna.iwanska@cabiri.io>
2025-01-05 12:50:17 +0000

    add player to drawable and updatable groups

--- main.py
+++ main.py
@@ -14,6 +14,10 @@ def main():
 
   player_init_x = SCREEN_WIDTH / 2
   player_init_y = SCREEN_HEIGHT / 2
+
+  updatable = pygame.sprite.Group()
+  drawable = pygame.sprite.Group()
+  Player.containers = (updatable, drawable)
   player = Player(player_init_x, player_init_y)
 
   while True:
@@ -22,9 +26,14 @@ def main():
           return
     dt_in_millis = clock.tick(60)
     delta_time = dt_in_millis / 1000
+    
     screen.fill("black")
-    player.draw(screen)
-    player.update(delta_time)
+
+    for to_draw in drawable:
+      to_draw.draw(screen) 
+    for to_update in updatable:
+      to_update.update(delta_time) 
+    
     pygame.display.flip()
 
 if __name__ == "__main__":
